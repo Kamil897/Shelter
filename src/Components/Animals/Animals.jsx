8213cf332585c624fbe303b9d170cfa2eda4a512
Kamil897 <kamil2007shakirov@gmail.com>
2025-03-01 17:07:24 +0500

    12

--- src/Components/Animals/Animals.jsx
+++ src/Components/Animals/Animals.jsx
@@ -4,15 +4,15 @@ import DecryptedText from "../DecryptedText/DecryptedText";
 import s from "./Animals.module.scss";
 
 const pets = [
-  { name: "Gary", gender: "male", age: 3, breed: "Yorkshire Terrier", image: "/public/cards/gary.jpg" },
-  { name: "Moon", gender: "female", age: 1, breed: "Siberian Cat", image: "/public/cards/moon.png" },
-  { name: "Spike", gender: "male", age: 2.5, breed: "Maine Coon", image: "/public/cards/spike.png" },
-  { name: "Peach", gender: "male", age: 5, breed: "Half Breed", image: "/public/cards/peach.png" },
-  { name: "Willie", gender: "female", age: 0.5, breed: "Samoyed", image: "/public/cards/willie.png" },
-  { name: "Kiwi", gender: "male", age: 4, breed: "Yorkshire Terrier", image: "/public/cards/kiwi.png" },
-  { name: "Whitney", gender: "female", age: 5, breed: "British Longhair", image: "/public/cards/white.png" },
-  { name: "Buggy", gender: "male", age: 7, breed: "Jack Russell Terrier", image: "/public/cards/buggi.png" },
-  { name: "Cake", gender: "male", age: 2, breed: "Welsh Corgi", image: "/public/cards/cake.png" },
+  { name: "Gary", gender: "male", age: 3, breed: "Yorkshire Terrier", image: "https://avatars.mds.yandex.net/i?id=0f2293c7f33a41e476f3da2068c854af783ba0e1-3703415-images-thumbs&n=13" },
+  { name: "Moon", gender: "female", age: 1, breed: "Siberian Cat", image: "https://storage-api.petstory.ru/resize/1000x1000x80/a1/5a/5d/a15a5d1b86084b07b31d0843eb8406c3.jpeg" },
+  { name: "Spike", gender: "male", age: 2.5, breed: "Maine Coon", image: "https://avatars.mds.yandex.net/i?id=8c818f4aec24084a41bc48a72449a2601c50b266-10233206-images-thumbs&n=13" },
+  { name: "Peach", gender: "male", age: 5, breed: "Half Breed", image: "https://avatars.mds.yandex.net/i?id=c911b359f9e08773a3e0224690d5c74ed639d286-3858847-images-thumbs&n=13" },
+  { name: "Willie", gender: "female", age: 0.5, breed: "Samoyed", image: "https://avatars.mds.yandex.net/i?id=1b0b62208862d4eeb1428679c563fc260936458f-10263675-images-thumbs&n=13" },
+  { name: "Kiwi", gender: "male", age: 4, breed: "Yorkshire Terrier", image: "https://avatars.mds.yandex.net/i?id=011058b7e36eb2d073dcfeb26324c208a030ec6fe7034201-12321751-images-thumbs&n=13" },
+  { name: "Whitney", gender: "female", age: 5, breed: "British Longhair", image: "https://avatars.mds.yandex.net/i?id=fc28b297b754769d2f72bf8439141ed6875083a6-10119783-images-thumbs&n=13" },
+  { name: "Buggy", gender: "male", age: 7, breed: "Jack Russell Terrier", image: "https://avatars.mds.yandex.net/i?id=6bb3fefc992eab1741b64fa655d58d98b9fac2ea-4010901-images-thumbs&n=13" },
+  { name: "Cake", gender: "male", age: 2, breed: "Welsh Corgi", image: "https://avatars.mds.yandex.net/i?id=4e41f25de07d96fcb7c3b86d87e2c1da-4903276-images-thumbs&n=13" },
 ];
 
 const Animals = () => {
--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -2,15 +2,15 @@ import s from "./Store.module.scss";
 import DecryptedText from "../DecryptedText/DecryptedText";
 
 const products = [
-  { name: "Fancy Feast", type: "Cat food", price: "35$", image: "/public/Store/fancy.png" },
-  { name: "Iams", type: "Cat food", price: "54$", image: "/public/Store/lams.png" },
-  { name: "Nulo", type: "Cat food", price: "62$", image: "/public/Store/nulo.png" },
-  { name: "Kibbles nBits", type: "Dog food", price: "28$", image: "/public/Store/kibbles.png" },
-  { name: "Cesar", type: "Dog food", price: "46$", image: "/public/Store/cesar.png" },
-  { name: "Happy dog", type: "Dog food", price: "76$", image: "/public/Store/happy.png" },
-  { name: "Wau dog", type: "Collar for dog", price: "10$", image: "/public/Store/wau.png" },
-  { name: "Wau cat", type: "Collar for cat", price: "7$", image: "/public/Store/wau_cat.png" },
-  { name: "Toy", type: "For dog and cat", price: "3$", image: "/public/Store/toy.png" }
+  { name: "Fancy Feast", type: "Cat food", price: "35$", image: "https://avatars.mds.yandex.net/i?id=22f4b72b9815e44e73d33f568c77083d61c7d703-12753086-images-thumbs&n=13" },
+  { name: "Iams", type: "Cat food", price: "54$", image: "https://avatars.mds.yandex.net/i?id=acd2ef79e4dab749b4f6daf4bf72cfbf36cfe8f0-8495786-images-thumbs&n=13" },
+  { name: "Nulo", type: "Cat food", price: "62$", image: "https://avatars.mds.yandex.net/i?id=5ca75fb1b8fa7ea451a7c067ab21b427dd789247-10310583-images-thumbs&n=13" },
+  { name: "Kibbles nBits", type: "Dog food", price: "28$", image: "https://avatars.mds.yandex.net/i?id=c346216b7ecbe1f0b57a23746f0cb965-4568063-images-thumbs&n=13" },
+  { name: "Cesar", type: "Dog food", price: "46$", image: "https://avatars.mds.yandex.net/i?id=294016fd909e5798ae5baa479ef76cfa7d2700ca-9097903-images-thumbs&n=13" },
+  { name: "Happy dog", type: "Dog food", price: "76$", image: "https://avatars.mds.yandex.net/i?id=5ae7a448f5711e1c7b3a73ca69a2a20d804f739d-12842814-images-thumbs&n=13" },
+  { name: "Wau dog", type: "Collar for dog", price: "10$", image: "https://avatars.mds.yandex.net/i?id=40181eb47bd502a7173c309ef49411ce0ee98a89-8922087-images-thumbs&n=13" },
+  { name: "Wau cat", type: "Collar for cat", price: "7$", image: "https://avatars.mds.yandex.net/i?id=6d5b74053689dc9847d26c9cf411ac278ff050fb-5255999-images-thumbs&n=13" },
+  { name: "Toy", type: "For dog and cat", price: "3$", image: "https://avatars.mds.yandex.net/i?id=17b7b64fbb97d085d7b0cc58d64a64ec6309b65a86669c50-11858942-images-thumbs&n=13" }
 ];
 
 export default function Store() {
